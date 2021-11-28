1d86b7376c8ba646e0c161736b67a5de40d71ccd
Gabriel Oliveira <gabrielscorehero55@gmail.com>
2021-11-28 00:47:30 -0300

    fix: replace start script

--- src/server.ts
+++ src/server.ts
@@ -18,6 +18,6 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(router);
 
-app.listen(process.env.PORT, () =>
+app.listen(process.env.PORT || 4000, () =>
   console.log(`Server is running on port ${process.env.PORT}`)
 );
